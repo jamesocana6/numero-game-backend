3ef3e87b9fcb01d513ae43693a43e7562e391aa5
James Ocana <ojam1996@gmail.com>
2023-03-17 18:43:55 -0400

    added things for build

--- controllers/highscores.js
+++ controllers/highscores.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const User = require("../models/user")
+const User = require("../models/User")
 const Highscore = require("../models/Highscore")
 const highscoreRouter = express.Router()
 const auth = require("../middleware/auth")
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const User = require("../models/user")
+const User = require("../models/User")
 const userRouter = express.Router()
 const bcrypt = require("bcrypt");
 const auth = require("../middleware/auth")
