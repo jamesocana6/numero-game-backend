4ffd7c2b97b93b28088f8bdb4f8123e38f80ac9c
James Ocana <ojam1996@gmail.com>
2023-10-17 03:02:12 -0400

    fixed highscores in model for user

--- models/User.js
+++ models/User.js
@@ -6,15 +6,9 @@ const userSchema = mongoose.Schema({
     password: { type: String, required: true },
     token: { type: String, },
     highscores: { 
-        hs24e30: { type: Number, default: 0 }, 
-        hs24m60: { type: Number, default: 0 }, 
-        hs24h120: { type: Number, default: 0 }, 
-        hs48e30: { type: Number, default: 0 }, 
-        hs48m60: { type: Number, default: 0 }, 
-        hs48h120: { type: Number, default: 0 }, 
-        hs60e30: { type: Number, default: 0 }, 
-        hs60m60: { type: Number, default: 0 }, 
-        hs60h120: { type: Number, default: 0 }, 
+        hse30: { type: Number, default: 0 }, 
+        hsm60: { type: Number, default: 0 }, 
+        hsh120: { type: Number, default: 0 }, 
     },
     // postedScore: [{type: mongoose.Schema.Types.ObjectId, ref: "Highscore"}],
 });
