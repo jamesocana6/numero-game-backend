f93e01b747ba3d5fb0c588eeb859760dfa0a657b
James Ocana <ojam1996@gmail.com>
2023-10-17 03:23:46 -0400

    fixed highscores in model for highscores

--- models/Highscore.js
+++ models/Highscore.js
@@ -3,7 +3,6 @@ let mongoose = require("mongoose")
 const highscoreSchema = mongoose.Schema({
     username: { type: String, required: true, },
     gameDifficulty: { type: String, required: true, },
-    gameTarget: { type: Number, required: true, },
     gameTime: { type: Number, required: true, },
     value: { type: Number, required: true, },
 });
